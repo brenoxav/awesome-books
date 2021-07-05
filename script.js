45c307561fc3bc9717820dc12aef8b945e27e624
Diego Solis <disc3110@gmail.com>
2021-07-05 16:50:28 -0500

    add remove funtionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const booksWrapper = document.querySelector('.books-wrapper');
 const titleInput = document.querySelector('#new-book-title');
 const authorInput = document.querySelector('#new-book-author');
 const addBtn = document.querySelector('.add-btn');
+const removeBtn = document.querySelector('.remove-btn')
+
 
 let books = [
     {
@@ -19,49 +21,55 @@ function addBook(title, author) {
         title: title,
         author: author
     }
+
     books.push(newBook)
     renderBooks(books);
 }
 
-function removeBook(title){
-    const newArray = books.filter(function(book) {
-        return book.title != title
-    })
-    books = newArray
+function removeBook(id) {
+    books.splice(id, 1)
+    renderBooks(books);
 }
 
 function renderBooks(arr) {
-  booksWrapper.innerHTML = '';
-  arr.forEach(book => {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
+    booksWrapper.innerHTML = '';
+    arr.forEach(function (book, index) {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
 
-    const bookTitle = document.createElement('p');
-    bookTitle.textContent = book.title;
-    bookTitle.classList.add('book-title');
+        const bookTitle = document.createElement('p');
+        bookTitle.textContent = book.title;
+        bookTitle.classList.add('book-title');
 
-    const bookAuthor = document.createElement('p');
-    bookAuthor.classList.add('book-author');
-    bookAuthor.textContent = book.author;
+        const bookAuthor = document.createElement('p');
+        bookAuthor.classList.add('book-author');
+        bookAuthor.textContent = book.author;
 
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove-btn');
-    removeBtn.textContent = 'Remove';
-    
-    bookDiv.appendChild(bookTitle);
-    bookDiv.appendChild(bookAuthor);
-    bookDiv.appendChild(removeBtn);
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-btn');
+        removeBtn.textContent = 'Remove';
+        removeBtn.setAttribute('data-id', index)
 
-    booksWrapper.appendChild(bookDiv);
-  });
+        removeBtn.addEventListener('click', function() {
+            const bookId = index
+            removeBook(bookId)
+
+        });
+
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(removeBtn);
+
+        booksWrapper.appendChild(bookDiv);
+    });
 }
 
-addBtn.addEventListener('click', function(){
-  const newBookTitle = titleInput.value;
-  const newBookAuthor = authorInput.value;
-  addBook(newBookTitle, newBookAuthor);
-  titleInput.value = '';
-  authorInput.value = '';
+addBtn.addEventListener('click', function () {
+    const newBookTitle = titleInput.value;
+    const newBookAuthor = authorInput.value;
+    addBook(newBookTitle, newBookAuthor);
+    titleInput.value = '';
+    authorInput.value = '';
 });
 
 renderBooks(books);
