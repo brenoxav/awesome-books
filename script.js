c5d3814fda4b2d9cae2829fde905da66c219bbe8
Breno Xavier <brenoxav@gmail.com>
2021-07-05 16:46:52 -0400

    Add renderBooks and addBook from form

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const booksWrapper = document.querySelector('.books-wrapper');
+const titleInput = document.querySelector('#new-book-title');
+const authorInput = document.querySelector('#new-book-author');
+const addBtn = document.querySelector('.add-btn');
+
 let books = [
     {
         title: "Sapiens: A Brief History of Humankind",
@@ -15,6 +20,7 @@ function addBook(title, author) {
         author: author
     }
     books.push(newBook)
+    renderBooks(books);
 }
 
 function removeBook(title){
@@ -23,3 +29,39 @@ function removeBook(title){
     })
     books = newArray
 }
+
+function renderBooks(arr) {
+  booksWrapper.innerHTML = '';
+  arr.forEach(book => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+
+    const bookTitle = document.createElement('p');
+    bookTitle.textContent = book.title;
+    bookTitle.classList.add('book-title');
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.classList.add('book-author');
+    bookAuthor.textContent = book.author;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.textContent = 'Remove';
+    
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(removeBtn);
+
+    booksWrapper.appendChild(bookDiv);
+  });
+}
+
+addBtn.addEventListener('click', function(){
+  const newBookTitle = titleInput.value;
+  const newBookAuthor = authorInput.value;
+  addBook(newBookTitle, newBookAuthor);
+  titleInput.value = '';
+  authorInput.value = '';
+});
+
+renderBooks(books);
