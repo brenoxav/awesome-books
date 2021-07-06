1a03d3010d404011d4dfbef19d8dc75e61f54aca
Breno Xavier <brenoxav@gmail.com>
2021-07-06 15:25:09 -0400

    Implement Book and Library classes

--- script.js
+++ script.js
@@ -3,26 +3,53 @@ const titleInput = document.querySelector('#new-book-title');
 const authorInput = document.querySelector('#new-book-author');
 const addBtn = document.querySelector('.add-btn');
 
-let books = [
-  {
-    title: 'Sapiens: A Brief History of Humankind',
-    author: 'Yuval Noah Harari',
-  },
-  {
-    title: 'Homo Deus',
-    author: 'Yuval Noah Harari',
-  },
-];
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
-function setStorage() {
-  localStorage.setItem('books', JSON.stringify(books));
+class Library {
+  constructor(){
+    this.books = [];
+  }
+
+  addBook(book) {
+    this.books.push(book);
+    return this.books;
+  }
+
+  removeBook(id) {
+    this.books = this.books.filter((book, index) => index !== id);
+    return this.books;
+  }
+
+  getBooks() {
+    return this.books;
+  }
+
+}
+
+let book1 = new Book('Sapiens: A Brief History of Humankind', 'Yuval Noah Harari');
+let book2 = new Book('Homo Deus', 'Yuval Noah Harari');
+
+let library = new Library;
+
+library.addBook(book1);
+library.addBook(book2);
+
+console.log(library.books);
+
+function setStorage(libraryArr) {
+  localStorage.setItem('library', JSON.stringify(libraryArr));
 }
 
 function getStorage() {
-  books = JSON.parse(localStorage.getItem('books'));
+  books = JSON.parse(localStorage.getItem('library'));
 }
 
-function updateStorage() {
+function checkStorage() {
   if (localStorage.length > 0) {
     getStorage();
   } else {
@@ -30,22 +57,22 @@ function updateStorage() {
   }
 }
 
-function addBook(title, author) {
-  const newBook = {
-    title,
-    author,
-  };
+// function addBook(title, author) {
+//   const newBook = {
+//     title,
+//     author,
+//   };
 
-  books.push(newBook);
-  setStorage();
-  renderBooks(books);// eslint-disable-line
-}
+//   books.push(newBook);
+//   setStorage();
+//   renderBooks(books);// eslint-disable-line
+// }
 
-function removeBook(id) {
-  books.splice(id, 1);
-  setStorage();
-  renderBooks(books);// eslint-disable-line
-}
+// function removeBook(id) {
+//   books.splice(id, 1);
+//   setStorage();
+//   renderBooks(books);// eslint-disable-line
+// }
 
 function renderBooks(arr) {
   booksWrapper.innerHTML = '';
@@ -68,7 +95,8 @@ function renderBooks(arr) {
 
     removeBtn.addEventListener('click', () => {
       const bookId = index;
-      removeBook(bookId);
+      library.removeBook(bookId);
+      renderBooks(library.getBooks());
     });
 
     bookDiv.appendChild(bookTitle);
@@ -82,10 +110,11 @@ function renderBooks(arr) {
 addBtn.addEventListener('click', () => {
   const newBookTitle = titleInput.value;
   const newBookAuthor = authorInput.value;
-  addBook(newBookTitle, newBookAuthor);
+  library.addBook(new Book(newBookTitle, newBookAuthor));
   titleInput.value = '';
   authorInput.value = '';
+  renderBooks(library.getBooks());
 });
 
-updateStorage();
-renderBooks(books);
+//checkStorage();
+renderBooks(library.getBooks());
